986199d76cd9f807f6fa1f42273b2724c0966932
alexfaller <alexfaller1234@gmail.com>
2023-06-22 19:43:14 -0300

    inicio

--- index.js
+++ index.js
@@ -20,30 +20,36 @@
 
 
 
-let codigo
-do {
-    codigo = prompt("inserte un codigo postal");
-    if (Number(codigo) == codigo) {
-        if (codigo > 0 && codigo <= 4000) {
-            if (codigo < 2000) {
-                alert("El envio es de $2000")
-            }
+// let codigo
+// do {
+//     codigo = prompt("inserte un codigo postal");
+//     if (Number(codigo) == codigo) {
+//         if (codigo > 0 && codigo <= 4000) {
+//             if (codigo < 2000) {
+//                 alert("El envio es de $2000")
+//             }
 
-            else if (codigo >= 2000) {
-                alert("El envio es gratis");
-            }
-        }
+//             else if (codigo >= 2000) {
+//                 alert("El envio es gratis");
+//             }
+//         }
 
-        else { alert("el codigo postal no es valido") };
-    }
-    else {
+//         else { alert("el codigo postal no es valido") };
+//     }
+//     else {
 
-        if (codigo !== undefined) {
-            alert("Introduce un numero valido");
-        }
-    }
-}
-while (codigo != undefined);
+//         if (codigo !== undefined) {
+//             alert("Introduce un numero valido");
+//         }
+//     }
+// }
+// while (codigo != undefined);
 
+// let dinero = prompt("ingrese cuando dinero tiene")
+// if (dinero < 10 ){alert("no te alcanza para nada")}
+
+
+
+// 0.6 1 1.6 1.7 1.8 2.9 2.9
 
 
